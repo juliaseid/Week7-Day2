1788e0bb0a77aa57816e81279b9ab4568a29327a
unknown <macjay314@gmail.com>
2020-05-05 16:29:22 -0700

    Fixed guess type errors. Still need to do finite number guess ie: be able to guess 2

---
+++ higher-lower-game/Game.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+class NumberGuesser
+{
+
+  static void Main()
+  {
+    Console.WriteLine("Do you want to play the number guessing game?");
+    string response = Console.ReadLine();
+    bool play = false;
+    if (response == "no") {
+      Console.WriteLine("Aww what a shame...");
+    } else if (response == "yes") {
+      play = true;
+      Console.WriteLine("Great! Think of a number between 1 and 100, exclusive");
+      int guess = 50;
+      string answer, end;
+      while (play == true) {
+      Console.WriteLine("Is your number higher than " + guess + "?");
+      answer = Console.ReadLine().Trim().ToLower();
+      if (answer == "yes"){
+        guess += guess / 2; 
+      } else if (answer == "no") {
+        guess /= 2;
+      } else if (answer == "correct") {
+        Console.WriteLine("Yay! I guessed it! Do you want to play again?");
+        end = Console.ReadLine();
+        if (end == "yes") {
+          guess = 50;
+        } else {
+          play = false;
+          }
+      } else {
+          Console.WriteLine("I'm sorry, I only understand: yes, no, correct. Please only enter one of those messages.");
+        }
+      }
+    }
+  }
+}
---
+++ higher-lower-game/README.md
@@ -0,0 +1,31 @@
+# _Epicodus C# Higher_
+
+#### _Date: May 5th, 2020
+#### By _**Jason Macie**, **Julia Seidman**_
+
+## Description
+
+_This is a game that will attempt to guess a number that the user is thinking of. The game will ask the user questions until it guesses the number correctly._
+
+## Setup/Installation Requirements
+
+This program requires .NET to be installed.
+* Clone this repository
+* Navigate to the repo in command console
+* Run: dotnet build
+* Run: dotnet run
+
+## Specs
+
+* Take in a string from the user
+* Create a method to test if that string is indeed a palindrome
+
+## Technologies Used
+
+* .NET
+
+### License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2020 **_Jason Macie_, _Julia Seidman_**
