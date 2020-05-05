da2db1fba2e96b5e9cca44674f3ef7240727c727
unknown <macjay314@gmail.com>
2020-05-05 11:37:36 -0700

    Initial functionality achieved. Continue to work on exceptions ie: handle spaces

---
+++ palindromes/Palindromes.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public class Palindrome {
+
+  public static bool checkPal(string input){
+    // char[] charArray = input.Split(Char[]);
+    char[] charArray = input.ToCharArray();
+    int j = charArray.Length - 1;
+    for (int i=0; i<charArray.Length; i++) {
+      if (charArray[i] != charArray[j]){
+        return false;
+      } 
+      j--;
+    }
+    return true;
+  }
+  public static void Main(){
+    Console.WriteLine("Please enter a palindrome");
+    string input = Console.ReadLine();
+    Console.WriteLine(checkPal(input));
+  }
+}
